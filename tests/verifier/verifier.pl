91b10ca095ea2efe6aaab830e34c2afe2c3e4cbf
Viacheslav Login <vlogin@nvidia.com>
2024-09-26 09:40:21 +0300

    [CI] Nvidia license and copyright information updated
    It was found that some files contained outdated licenses and copyright information.
    This information in the file headers was updated using the NGCI v5.0.3-82 Header Check utility.
    
    Issue: HPCINFRA-2612
    Signed-off-by: Viacheslav Login <vlogin@nvidia.com>

--- tests/verifier/verifier.pl
+++ tests/verifier/verifier.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/env perl
 #
+# SPDX-FileCopyrightText: NVIDIA CORPORATION & AFFILIATES
 # Copyright (c) 2011-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # All rights reserved.
 #
